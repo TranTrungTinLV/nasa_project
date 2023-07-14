21adc70d4c8f5a3a6d191dafea9ff43e553f9d09
TranTrungTinLV <trungtintin1989@gmail.com>
2023-07-14 23:31:53 +0700

    update code

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,8 +1,11 @@
-const planets = require('../../models/planets.model');
+const {
+    planets
+} = require('../../models/planets.model');
+
 function getAllPlannets(req, res) {
     return res.status(200).json(planets)
 }
 
 module.exports = {
-    getAllPlannets, 
+    getAllPlannets,
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,20 @@
 const http = require('http');
 const PORT = process.env.PORT || 8000;
-const app = require('./app')
-const server = http.createServer(app)
-server.listen(PORT, () => {
-    console.log(
-        `Listening on Port ${PORT}...`
-    )
-});
+const app = require('./app');
+const {
+    loadPlanets
+} = require('./models/planets.model');
+const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanets();
+    server.listen(PORT, () => {
+        console.log(
+            `Listening on Port ${PORT}...`
+        )
+    });
+}
+
+startServer();
+
 console.log(PORT)
